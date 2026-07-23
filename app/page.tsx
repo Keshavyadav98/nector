174fca04e5ac6aa103ee40725331f3e122402ecc
keshav98 <1998keshavyadav@gmail.com>
2026-07-23 08:39:51 +0530

    page added

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen bg-[linear-gradient(90deg,#8B6BA8_0%,#76539D_25%,#7026A8_50%,#4B197F_70%,#241047_100%)]">
+    <main className="w-full min-h-screen bg-[#01000A]">
       <img
         src="/homePage.png"
         alt="Homepage"
